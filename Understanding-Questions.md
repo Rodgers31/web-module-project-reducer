e3cdd43644907a39581722b7cb60b32f6bd05b2b
Rodgers Otieno <otienor@uindy.edu>
2021-07-13 11:06:10 -0400

    Only add to memory is left

--- Understanding-Questions.md
+++ Understanding-Questions.md
@@ -1,7 +1,9 @@
 # Understanding Questions:
-1. What are the steps of execution from the pressing of the 1 button to the rendering of our updated value? List what part of the code excutes for each step.
-* The user presses the 1 button.
-* 
-...
 
-* TotalDisplay shows the total plus 1.
+1. What are the steps of execution from the pressing of the 1 button to the rendering of our updated value? List what part of the code excutes for each step.
+
+- The user presses the 1 button.
+- The handleEvent is clicke and dispached is runned, it finds an action creator called addOne that it's called. It's goes into the addone's origin in the actions folder finds the creation and origin which is in the reducer folder and finds where the operations took place
+  ...
+
+- TotalDisplay shows the total plus 1.
--- src/App.js
+++ src/App.js
@@ -1,67 +1,90 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 
 import './App.css';
 
 import TotalDisplay from './components/TotalDisplay';
 import CalcButton from './components/CalcButton';
+import reducer, { initialState, calculateResult } from './reducers';
+import {
+	addOne,
+	applyNumber,
+	changeOperator,
+	clearDisplay,
+} from './actions/index';
 
 function App() {
+	const [state, dispatch] = useReducer(reducer, initialState);
+	const handleEvent = (e) => {
+		// dispatch(addOne());
+		dispatch(applyNumber(e));
+	};
 
-  return (
-    <div className="App">
-      <nav className="navbar navbar-dark bg-dark">
-        <a className="navbar-brand" href="#"><img width="40px" src="./Lambda-Logo-Red.png"/> Lambda Reducer Challenge</a>
-      </nav>
+	const handleOperator = (e) => {
+		dispatch(changeOperator(e));
+	};
+	const clearHandler = (e) => {
+		dispatch(clearDisplay(e));
+	};
+	return (
+		<div className='App'>
+			<nav className='navbar navbar-dark bg-dark'>
+				<a className='navbar-brand' href='#'>
+					<img width='40px' src='./Lambda-Logo-Red.png' /> Lambda Reducer
+					Challenge
+				</a>
+			</nav>
 
-      <div className = "container row mt-5">
-        <div className="col-md-12 d-flex justify-content-center">
-          <form name="Cal">
-            
-            <TotalDisplay value={0}/>
-            <div className="row details">
-              <span id="operation"><b>Operation:</b> X</span>
-              <span id="memory"><b>Memory:</b> 0</span>
-            </div>
-            
-            <div className="row">
-              <CalcButton value={"M+"}/>
-              <CalcButton value={"MR"}/>
-              <CalcButton value={"MC"}/>
-            </div>
+			<div className='container row mt-5'>
+				<div className='col-md-12 d-flex justify-content-center'>
+					<form name='Cal'>
+						<TotalDisplay value={state.total} />
+						<div className='row details'>
+							<span id='operation'>
+								<b>Operation:</b> {state.operation}
+							</span>
+							<span id='memory'>
+								<b>Memory:</b> {state.memory}
+							</span>
+						</div>
 
-            <div className="row">
-              <CalcButton value={1}/>
-              <CalcButton value={2}/>
-              <CalcButton value={3}/>
-            </div>
+						<div className='row'>
+							<CalcButton value={'M+'} />
+							<CalcButton value={'MR'} />
+							<CalcButton value={'MC'} />
+						</div>
 
-            <div className="row">
-              <CalcButton value={4}/>
-              <CalcButton value={5}/>
-              <CalcButton value={6}/>
-            </div>
+						<div className='row'>
+							<CalcButton value={1} onClick={(e) => handleEvent(1)} />
+							<CalcButton value={2} onClick={(e) => handleEvent(2)} />
+							<CalcButton value={3} onClick={(e) => handleEvent(3)} />
+						</div>
 
-            <div className="row">
-              <CalcButton value={7}/>
-              <CalcButton value={8}/>
-              <CalcButton value={9}/>
-            </div>
+						<div className='row'>
+							<CalcButton value={4} onClick={(e) => handleEvent(4)} />
+							<CalcButton value={5} onClick={(e) => handleEvent(5)} />
+							<CalcButton value={6} onClick={(e) => handleEvent(6)} />
+						</div>
 
-            <div className="row">
-              <CalcButton value={"+"}/>
-              <CalcButton value={"*"}/>
-              <CalcButton value={"-"}/>
-            </div>
+						<div className='row'>
+							<CalcButton value={7} onClick={(e) => handleEvent(7)} />
+							<CalcButton value={8} onClick={(e) => handleEvent(8)} />
+							<CalcButton value={9} onClick={(e) => handleEvent(9)} />
+						</div>
 
-            <div className="row ce_button">
-              <CalcButton value={"CE"}/>
-            </div>
+						<div className='row'>
+							<CalcButton value={'+'} onClick={(e) => handleOperator('+')} />
+							<CalcButton value={'*'} onClick={(e) => handleOperator('*')} />
+							<CalcButton value={'-'} onClick={(e) => handleOperator('-')} />
+						</div>
 
-          </form>
-        </div>
-      </div>
-    </div>
-  );
+						<div className='row ce_button'>
+							<CalcButton value={'CE'} onClick={(e) => clearHandler('CE')} />
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
